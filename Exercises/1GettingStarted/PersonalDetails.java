b1c62ce42c169b02dd8faced5369d56e73b511df
shxziia <shxziia@gmail.com>
2026-05-27 16:14:51 +0100

    Add files via upload

---
+++ Exercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,15 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+
+        String name = "Shazia Begum";
+        String address = "Leeds Beckett";
+        int age = 20;
+        long phone = 127437721;
+
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phone);
+    }
+    }
+
---
+++ Exercises/1GettingStarted/S.java
@@ -0,0 +1,6 @@
+public class S {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.print("\thi");
+    }
+}
